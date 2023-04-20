c6d4419095714c308ebdc0eeb71f2e085927e896
RodrigoFA216 <flores.rodrigo.emmanuel.dev@gmail.com>
2023-04-19 21:22:37 -0500

    added verify

--- app/routes/router.py
+++ app/routes/router.py
@@ -7,7 +7,7 @@ import shutil
 from dotenv import load_dotenv
 
 from app.schemas.item_scheme import ItemScheme
-from app.functions import divide_img
+from app.functions import divide_img, verify
 
 router = APIRouter()
 
@@ -19,9 +19,8 @@ imgCifFolder = "app/temp/imgCif/"
 imgFormats = (".png", ".jpg", ".bmp")
 
 
-@router.post("/API/Encrypt/Image", tags=["Post", "Recive Imagen", "encrypt"])
+@router.post("/API/Encrypt/Image", tags=["Recive Imagen"])
 async def reciveImage(file: UploadFile = File(...)):
-    try:
         if file.filename[-4:] in imgFormats:
             # Uno la ruta de imgFolder con el nombre del archivo menos la extensión
             file_folder = os.path.join(imgFolder, file.filename[:-4])
@@ -33,16 +32,20 @@ async def reciveImage(file: UploadFile = File(...)):
                 content = await file.read()
                 F.write(content)
                 F.close()
-            # uso la función divide_img para dividir los componentes
-            res_divide = divide_img.divide(file_path, file.filename)
-            # Respondo un archivo con la dirección de guardado
-            return FileResponse(res_divide["img_ycrfile"])
+            valid_image = verify.verify_image_color(file_path)
+            if valid_image:
+                # uso la función divide_img para dividir los componentes
+                res_divide = divide_img.divide(file_path, file.filename)
+                # Respondo un archivo con la dirección de guardado
+                return FileResponse(res_divide["img_ycrfile"])
+            else:
+                return JSONResponse(
+                    {"error": "La imágen no puede ser procesada de forma correcta debido a que no tiene n la cantidad de canales adecuada"},
+                    status_code=200
+                )
         else:
             return JSONResponse(
                 content={"Error": "La extención del archivo no es válida"},
-                status_code=200,
+                status_code=200
             )
-    except:
-        return JSONResponse(
-            content={"Error": "Algo falló con el archivo"}, status_code=205
-        )
+    
